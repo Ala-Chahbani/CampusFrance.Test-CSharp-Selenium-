4000349161a623d077a037099f6547e2bc667581
Alaeddine Chahbani <alaeddine.chahbani@gmail.com>
2025-09-16 14:21:45 +0200

    préparation du driver pour les tests de creation d'étudiant

--- GestionDeCompte/Creation/CreationEtudiant.cs
+++ GestionDeCompte/Creation/CreationEtudiant.cs
@@ -1,16 +1,35 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
 namespace CampusFrance.Test.GestionDeCompte.Creation
 {
     public class CreationEtudiant
     {
-        [SetUp]
-        public void Setup()
+        // gère le driver qui transmet les commandes utilisateurs au navigateur Edge
+        private readonly IWebDriver Driver = new EdgeDriver();
+
+        // l'url de la page de création
+        private readonly string UrlPageCreation = "https://www.campusfrance.org/fr/user/register";
+
+        //  prépare l'exécution de l'ensemble des tests de la classe CreationEtudiant
+        [OneTimeSetUp]
+        public void Preparation()
         {
+            // maximise la fenêtre lancée par le driver
+            Driver.Manage().Window.Maximize();
         }
 
         [Test]
-        public void CreationEtudiantMathematiquesLicence3()
+        public void RenseignementEtudiantMathematiquesLicence3()
         {
-            Assert.Pass();
+            Assert.Ignore();
+        }
+
+        [OneTimeTearDown]
+        public void Fin()
+        {
+            Driver.Quit();
+            Driver.Dispose();
         }
     }
 }

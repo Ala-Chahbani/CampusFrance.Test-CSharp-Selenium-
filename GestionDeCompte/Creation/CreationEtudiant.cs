412c7bf82e373f552e4f2fed5f40a70d060dd964
Alaeddine Chahbani <alaeddine.chahbani@gmail.com>
2025-09-16 14:02:24 +0200

    Add project files.

---
+++ GestionDeCompte/Creation/CreationEtudiant.cs
@@ -0,0 +1,16 @@
+namespace CampusFrance.Test.GestionDeCompte.Creation
+{
+    public class CreationEtudiant
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreationEtudiantMathematiquesLicence3()
+        {
+            Assert.Pass();
+        }
+    }
+}
